2dd19c73313b0957ac90cf170c948d32f75ef76a
AriyanMlzm <ryanmolazem@gmail.com>
2024-05-04 17:28:49 +0330

    add matplotlib

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 from convertBW import *
 from convertFourier import *
@@ -16,20 +17,34 @@ convertBW(bw)
 magnitude = convertFourier(gray, False)
 log = convertFourier(gray, True)
 
-cv2.imshow('Color', image)
-resultsImg1 = np.concatenate((gray, bw), axis=1)
-cv2.imshow('results', resultsImg1)
-
-resultsImg2 = np.concatenate((magnitude, log), axis=1)
-cv2.imshow('results2', resultsImg2)
-
 image2 = cv2.imread("input2.png")
-resized_image2 = cv2.resize(image2, (512, 512))
-gray2 = cv2.cvtColor(resized_image2, cv2.COLOR_BGR2GRAY)
-con = gray2.copy()
-contrast(con)
-resultsImg3 = np.concatenate((gray2, con), axis=1)
-cv2.imshow('results3', resultsImg3)
+image2 = cv2.resize(image2, (512, 512))
+gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+contrast(gray2)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+fig = plt.figure(figsize=(12, 8))
+fig.add_subplot(2, 3, 1)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title('color')
+fig.add_subplot(2, 3, 2)
+plt.imshow(gray, cmap='gray')
+plt.title('gray')
+fig.add_subplot(2, 3, 3)
+plt.imshow(bw, cmap='gray')
+plt.title('bw')
+fig.add_subplot(2, 3, 4)
+plt.imshow(magnitude, cmap='gray')
+plt.title('fourier transform')
+fig.add_subplot(2, 3, 5)
+plt.imshow(log, cmap='gray')
+plt.title('log transform')
+
+fig = plt.figure(figsize=(8, 6))
+fig.add_subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(image2, cv2.COLOR_BGR2RGB))
+plt.title('gray')
+fig.add_subplot(1, 2, 2)
+plt.imshow(gray2, cmap='gray')
+plt.title('contrast')
+
+plt.show()
