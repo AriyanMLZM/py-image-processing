ccd51b18d8024ac65e360304bb961bf48e43c973
AriyanMlzm <ryanmolazem@gmail.com>
2024-05-08 23:38:36 +0330

    add histogram eq

--- figures/figure2.py
+++ figures/figure2.py
@@ -4,7 +4,7 @@ def showFig2(image1, image2):
   fig = plt.figure(figsize=(8, 6))
 
   fig.add_subplot(1, 2, 1)
-  plt.imshow(image1, "gray")
+  plt.imshow(image1)
   plt.title('gray')
   
   fig.add_subplot(1, 2, 2)
--- transformers/histogram.py
+++ transformers/histogram.py
@@ -1,6 +1,8 @@
 import cv2
 
 def histogram(image):
-  hist = cv2.calcHist([image], [0], None, [256], [0, 256])
-
-  return hist
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+  eq = cv2.equalizeHist(gray)
+  histeq = cv2.calcHist([eq], [0], None, [256], [0, 256])
+  return [hist, eq, histeq]
