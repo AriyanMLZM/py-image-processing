8450d3df2ebb885bf119588cac37d94122339634
AriyanMlzm <ryanmolazem@gmail.com>
2024-05-08 23:43:32 +0330

    clean up

--- transformers/histogram.py
+++ transformers/histogram.py
@@ -3,6 +3,8 @@ import cv2
 def histogram(image):
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+  
   eq = cv2.equalizeHist(gray)
   histeq = cv2.calcHist([eq], [0], None, [256], [0, 256])
+  
   return [hist, eq, histeq]
